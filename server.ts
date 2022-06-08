f6a5d3f947ce3f519b60124bc4bbe5babb7e4431
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2022-06-08 22:16:46 +0600

    initial commit

---
+++ server.ts
@@ -0,0 +1,31 @@
+import express, { Express, Request, Response } from "express";
+import cors, { CorsOptions } from "cors";
+import "dotenv/config";
+const app: Express = express();
+
+// CORS
+const allowedOrigins = ["http://localhost:3000"];
+
+const options: CorsOptions = {
+  origin: allowedOrigins,
+};
+
+// root middleware
+app.use(cors(options));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// routes
+// ..
+// ..
+// ..
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("<h1>Express + TypeScript Server</h1>");
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running at http://localhost:${PORT}`);
+});

5c26be57f3be05bfaf62bd95af3a08f4f7892673
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2022-09-16 16:39:09 +0600

    Empty App folder with files added

--- server.ts
+++ server.ts
@@ -32,5 +32,5 @@ app.get("/", (req: Request, res: Response) => {
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-  console.log(`[server]: Server is running at http://localhost:${PORT}`);
+  console.log(`[server] ⚡ : Server is running at http://localhost:${PORT}`);
 });
